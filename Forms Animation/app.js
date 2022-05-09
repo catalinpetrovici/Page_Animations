4682bb04b240cab661159b295a89ef293076b447
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-05-09 15:18:59 +0300

    Forms Animation - done - Checkbox / Character

--- Forms Animation/app.js
+++ Forms Animation/app.js
@@ -14,7 +14,6 @@ containers.forEach((container) => {
   const input = container.querySelector('.input');
   const line = container.querySelector('.elastic-line ');
   const placeholder = container.querySelector('.placeholder');
-  console.log('da');
 
   input.addEventListener('focus', () => {
     //Check to see if there is any text in the input
@@ -76,6 +75,83 @@ form.addEventListener('click', () => {
       }
     });
   });
+});
+
+// Checkbox animation fill
+const checkbox = document.querySelector('.checkbox');
+const tl2 = gsap.timeline({
+  defaults: { duration: 0.5, ease: 'Power2.easeOut' },
+});
+const tickMarkPath = document.querySelector('.tick-mark path');
+const pathLength = tickMarkPath.getTotalLength();
+
+gsap.set(tickMarkPath, {
+  strokeDashoffset: pathLength,
+  strokeDasharray: pathLength,
+});
+
+checkbox.addEventListener('click', () => {
+  console.log(checkbox.checked);
+  if (checkbox.checked) {
+    tl2.to('.checkbox-fill', { top: '0%' });
+    tl2.fromTo(
+      tickMarkPath,
+      { strokeDashoffset: pathLength },
+      { strokeDashoffset: 0 },
+      '<70%'
+    );
+    tl2.to('.checkbox-label', { color: '#6391e8' }, '<');
+  } else {
+    tl2.fromTo(
+      tickMarkPath,
+      { strokeDashoffset: 0 },
+      { strokeDashoffset: pathLength }
+    );
+    tl2.to('.checkbox-fill', { top: '100%' });
+    tl2.to('.checkbox-label', { color: '#c5c5c5' }, '<');
+  }
+});
+
+//Character Animation
+gsap.set('#eye', { transformOrigin: 'center' });
+gsap.fromTo(
+  '#eye',
+  { scaleY: 1 },
+  {
+    scaleY: 0.5,
+    repeat: -1,
+    yoyo: true,
+    repeatDelay: 0.5,
+    ease: 'Power2.easeOut',
+  }
+);
+gsap.fromTo(
+  '#eyebrow',
+  { y: 0 },
+  { y: -1, repeat: -1, yoyo: true, repeatDelay: 0.5, ease: 'Power2.easeOut' }
+);
+
+//Submit button
+const button = document.querySelector('button');
+const tl3 = gsap.timeline({
+  defaults: { duration: 0.75, ease: 'Power2.easeOut' },
+});
+
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+  tl3.to('.contact-right, .contact-left', {
+    y: 30,
+    opacity: 0,
+    pointerEvents: 'none',
+  });
+  tl3.to('form', { scale: 0.8 }, '<');
+  tl3.fromTo('.submitted', { opacity: 0, y: 30 }, { opacity: 1, y: 0 });
+  gsap.set('#hand', { transformOrigin: 'left' });
+  gsap.fromTo(
+    '#hand',
+    { rotation: 0, y: 0 },
+    { rotation: -10, y: 2, ease: 'elastic(3,0.3)', duration: 2, delay: 1 }
+  );
 });
 
 // checking email validation
