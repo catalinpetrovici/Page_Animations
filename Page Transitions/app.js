a6e551264e1bfc2c742f9cd02d79aa299fefe533
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-05-10 17:55:09 +0300

    Page Transitions - Leave and Enter Animation

--- Page Transitions/app.js
+++ Page Transitions/app.js
@@ -1,4 +1,74 @@
-//Run animations
+const tlEnter = gsap.timeline({
+  defaults: { duration: 0.75, ease: 'Power2.easeOut' },
+});
+
+const tlLeave = gsap.timeline({
+  defaults: { duration: 0.75, ease: 'Power2.easeOut' },
+});
+
+// Make the functions for the leave and enter animations
+
+const leaveAnimation = (current, done) => {
+  const product = current.querySelector('.image-container');
+  const text = current.querySelector('.showcase-text');
+  const circles = current.querySelectorAll('.circle');
+  const arrow = current.querySelector('.showcase-arrow');
+
+  return (
+    tlLeave.fromTo(arrow, { opacity: 1, y: 0 }, { opacity: 0, y: 50 }),
+    tlLeave.fromTo(
+      product,
+      { opacity: 1, y: 0 },
+      { opacity: 0, y: -100, onComplete: done },
+      '<'
+    ),
+    tlLeave.fromTo(text, { opacity: 1, y: 0 }, { opacity: 0, y: 100 }, '<'),
+    tlLeave.fromTo(
+      circles,
+      { opacity: 1, y: 0 },
+      {
+        opacity: 0,
+        y: -200,
+        stagger: 0.15,
+        ease: 'back.out(1.7)',
+        duration: 0.75,
+      },
+      '<'
+    )
+  );
+};
+
+const enterAnimation = (current, done) => {
+  const product = current.querySelector('.image-container');
+  const text = current.querySelector('.showcase-text');
+  const circles = current.querySelectorAll('.circle');
+  const arrow = current.querySelector('.showcase-arrow');
+
+  return (
+    tlLeave.fromTo(arrow, { opacity: 0, y: 50 }, { opacity: 1, y: 0 }),
+    tlLeave.fromTo(
+      product,
+      { opacity: 0, y: -100 },
+      { opacity: 1, y: 0, onComplete: done },
+      '<'
+    ),
+    tlLeave.fromTo(text, { opacity: 0, y: 100 }, { opacity: 1, y: 0 }, '<'),
+    tlLeave.fromTo(
+      circles,
+      { opacity: 0, y: -200 },
+      {
+        opacity: 1,
+        y: 0,
+        stagger: 0.15,
+        ease: 'back.out(1.7)',
+        duration: 0.75,
+      },
+      '<'
+    )
+  );
+};
+
+// Run animations
 
 barba.init({
   preventRunning: true,
@@ -9,21 +79,13 @@ barba.init({
       leave(data) {
         const done = this.async();
         let current = data.current.container;
-        gsap.fromTo(
-          current,
-          { opacity: 1 },
-          { opacity: 0, duration: 0.75, onComplete: done }
-        );
+        leaveAnimation(current, done);
       },
       enter(data) {
         const done = this.async();
 
         let next = data.next.container;
-        gsap.fromTo(
-          next,
-          { opacity: 0 },
-          { opacity: 1, duration: 0.75, onComplete: done }
-        );
+        enterAnimation(next, done);
       },
     },
   ],
