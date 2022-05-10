e1d79f5a6ecfc076961077856a6e09eea13b2a0b
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-05-10 18:13:39 +0300

    Page Transitions - Background Gradient Animation

--- Page Transitions/app.js
+++ Page Transitions/app.js
@@ -38,22 +38,23 @@ const leaveAnimation = (current, done) => {
   );
 };
 
-const enterAnimation = (current, done) => {
+const enterAnimation = (current, done, gradient) => {
   const product = current.querySelector('.image-container');
   const text = current.querySelector('.showcase-text');
   const circles = current.querySelectorAll('.circle');
   const arrow = current.querySelector('.showcase-arrow');
 
   return (
-    tlLeave.fromTo(arrow, { opacity: 0, y: 50 }, { opacity: 1, y: 0 }),
-    tlLeave.fromTo(
+    tlEnter.fromTo(arrow, { opacity: 0, y: 50 }, { opacity: 1, y: 0 }),
+    tlEnter.to('body', { background: gradient }, '<'),
+    tlEnter.fromTo(
       product,
       { opacity: 0, y: -100 },
       { opacity: 1, y: 0, onComplete: done },
       '<'
     ),
-    tlLeave.fromTo(text, { opacity: 0, y: 100 }, { opacity: 1, y: 0 }, '<'),
-    tlLeave.fromTo(
+    tlEnter.fromTo(text, { opacity: 0, y: 100 }, { opacity: 1, y: 0 }, '<'),
+    tlEnter.fromTo(
       circles,
       { opacity: 0, y: -200 },
       {
@@ -76,6 +77,13 @@ barba.init({
     //showcase transitions
     {
       name: 'default',
+      once(data) {
+        const done = this.async();
+        let next = data.next.container;
+        let gradient = getGradient(data.next.namespace);
+        gsap.set('body', { background: gradient });
+        enterAnimation(next, done, gradient);
+      },
       leave(data) {
         const done = this.async();
         let current = data.current.container;
@@ -83,10 +91,24 @@ barba.init({
       },
       enter(data) {
         const done = this.async();
-
         let next = data.next.container;
-        enterAnimation(next, done);
+        // get the barba namespace
+        let gradient = getGradient(data.next.namespace);
+        enterAnimation(next, done, gradient);
       },
     },
   ],
 });
+
+// Changing gradient on showcase
+
+function getGradient(name) {
+  switch (name) {
+    case 'handbag':
+      return 'linear-gradient(260deg, #b75d62, #754d4f)';
+    case 'boot':
+      return 'linear-gradient(260deg, #5d8cb7, #4c4f70)';
+    case 'hat':
+      return 'linear-gradient(260deg, #b27a5c, #7f5450)';
+  }
+}
