33769438d778bcfb71b685927df5dc5393043e01
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-05-12 14:46:40 +0300

    Ultimate Animation - Parallax

--- Ultimate Animation/app.js
+++ Ultimate Animation/app.js
@@ -152,6 +152,23 @@ tlVideo.fromTo(
 tlVideo.fromTo(
   '.product-info-container h3',
   { opacity: 0 },
-  { opacity: 1, stagger: 0.25, duration: 0.5 }
-),
-  '<';
+  { opacity: 1, stagger: 0.25, duration: 0.5 },
+  '<'
+);
+
+// 6th Page
+// Parallax
+
+const tlParallax = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.sixth-page',
+    markers: true,
+    start: '-25%',
+    end: '50%',
+    scrub: true,
+  },
+});
+
+tlParallax.fromTo('.photo-description', { y: 0 }, { y: -80 });
+tlParallax.fromTo('.portrait-container', { y: 0 }, { y: -80 }, '<');
+tlParallax.fromTo('.phone-video', { y: 0 }, { y: -250 }, '<');
