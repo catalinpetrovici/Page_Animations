8ae1aa89f8c5abc775a2741dc6fe6f03cc19bbdf
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-05-12 13:34:33 +0300

    Ultimate Animation - Video on scroll

--- Ultimate Animation/app.js
+++ Ultimate Animation/app.js
@@ -129,3 +129,29 @@ swatches.forEach((swatch, index) => {
     currentSwatch = swatchName;
   });
 });
+
+// Page 5 video on scroll
+
+const tlVideo = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.fifth-page',
+    start: '0%',
+    markers: true,
+    end: '150%',
+    scrub: true,
+    pin: true,
+  },
+});
+
+tlVideo.fromTo(
+  '.product-video',
+  { currentTime: 0 },
+  { currentTime: 3, duration: 1 }
+);
+
+tlVideo.fromTo(
+  '.product-info-container h3',
+  { opacity: 0 },
+  { opacity: 1, stagger: 0.25, duration: 0.5 }
+),
+  '<';
