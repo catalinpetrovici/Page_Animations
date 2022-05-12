9e66329f689732dced6c8b84a98dd6a04bd9842c
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-05-12 14:57:30 +0300

    Ultimate Animation - Remove markers

--- Ultimate Animation/app.js
+++ Ultimate Animation/app.js
@@ -29,7 +29,6 @@ const tlIntro = gsap.timeline({
 const tlH = gsap.timeline({
   scrollTrigger: {
     trigger: '.second-page',
-    markers: { startColor: 'white', endColor: 'white' },
     scrub: true,
     start: '-30%',
     end: '40%',
@@ -45,7 +44,6 @@ tlH.fromTo(
 const tlHRemove = gsap.timeline({
   scrollTrigger: {
     trigger: '.second-page',
-    markers: { startColor: 'pink', endColor: 'pink' },
     scrub: true,
     start: '-20%',
     end: '60%',
@@ -65,7 +63,6 @@ const tlSplit = gsap.timeline({
     trigger: '.third-page',
     start: '-25%',
     end: '30%',
-    markers: true,
     scrub: true,
   },
 });
@@ -136,7 +133,6 @@ const tlVideo = gsap.timeline({
   scrollTrigger: {
     trigger: '.fifth-page',
     start: '0%',
-    markers: true,
     end: '150%',
     scrub: true,
     pin: true,
@@ -162,7 +158,6 @@ tlVideo.fromTo(
 const tlParallax = gsap.timeline({
   scrollTrigger: {
     trigger: '.sixth-page',
-    markers: true,
     start: '-25%',
     end: '50%',
     scrub: true,
